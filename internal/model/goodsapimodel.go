d6a88721cc53a685b363569809b96a14d909220c
wdcyxxycdw <c_yuxuan@foxmail.com>
2024-10-09 17:06:55 +0900

    统一字段注解

--- internal/model/goodsapimodel.go
+++ internal/model/goodsapimodel.go
@@ -16,30 +16,30 @@ type UserForGetGoodsResponse struct {
 
 // receive this when frontend posts or updates goods
 type PostGoodsReceive struct {
-	Title       string
-	Description string
-	Images      []string
-	Price       float64
-	Tags        []string
-	SellerID    uint    `json:"seller_id"`
-	IsInvisible bool    `json:"is_invisible"`
-	IsDeleted   bool    `json:"is_deleted"`
-	IsBought    bool    `json:"is_bought"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Images      []string `json:"images"`
+	Price       float64  `json:"price"`
+	Tags        []string `json:"tags"`
+	SellerID    uint     `json:"seller_id"`
+	IsInvisible bool     `json:"is_invisible"`
+	IsDeleted   bool     `json:"is_deleted"`
+	IsBought    bool     `json:"is_bought"`
 }
 
 // send this when frontend posts or updates goods
 type PostGoodsResponse struct {
-	Message  string
-	GoodInfo Good
+	Message  string `json:"message"`
+	GoodInfo Good   `json:"good_info"`
 }
 
 // receive this when frontend deletes goods
 type DeleteGoodsReceive struct {
-	ID       uint
+	ID       uint `json:"id"`
 	SellerID uint `json:"seller_id"`
 }
 
 // send this when frontend deletes goods
 type DeleteGoodsResponse struct {
-	Message string
+	Message string `json:"message"`
 }
