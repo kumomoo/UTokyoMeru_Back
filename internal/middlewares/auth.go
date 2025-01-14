9d62c97a06c163469263a35cbff6bb67ebfe09f8
wdcyxxycdw <c_yuxuan@foxmail.com>
2025-01-14 11:11:41 +0900

    change file name

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -13,10 +13,8 @@ const ContextUserIDKey = "userID"
 // JWTAuthMiddleware 基于JWT的认证中间件
 func JWTAuthMiddleware() func(c *gin.Context) {
 	return func(c *gin.Context) {
-		// 客户端携带Token有三种方式 1.放在请求头 2.放在请求体 3.放在URI
-		// 这里假设Token放在Header的Authorization中，并使用Bearer开头
+		// 这里Token放在Header的Authorization中，并使用Bearer开头
 		// Authorization: Bearer xxxxxxx.xxx.xxx
-		// 这里的具体实现方式要依据你的实际业务情况决定
 		authHeader := c.Request.Header.Get("Authorization")
 		if authHeader == "" {
 			c.JSON(403, gin.H{"message": "Need login"})
@@ -74,3 +72,5 @@ func AdminAuthMiddleware() func(c *gin.Context) {
 		}
 	}
 }
+
+
